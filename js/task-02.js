f9b17f08d41cc260af136a404fed9ce6bcc7f871
sergeysheva32 <sergeysheva32@gmail.com>
2023-09-03 17:43:52 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,42 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector("#ingredients");
+
+
+const oneIngr = document.createElement("li");
+oneIngr.textContent = `Potatoes`;
+oneIngr.classList.add("item")
+console.log(oneIngr);
+list.append(oneIngr);
+
+const twoIngr = document.createElement("li");
+twoIngr.textContent = `Mushrooms`;
+twoIngr.classList.add("item")
+console.log(twoIngr);
+list.append(twoIngr);
+
+const threeIngr = document.createElement("li");
+threeIngr.textContent = `Garlic`;
+threeIngr.classList.add("item")
+console.log(threeIngr);
+list.append(threeIngr);
+
+const fourIngr = document.createElement("li");
+fourIngr.textContent = `Tomatos`;
+fourIngr.classList.add("item")
+console.log(fourIngr);
+list.append(fourIngr);
+
+const fiveIngr = document.createElement("li");
+fiveIngr.textContent = `Herbs`;
+fiveIngr.classList.add("item")
+console.log(fiveIngr);
+list.append(fiveIngr);
+
+const sixIngr = document.createElement("li");
+sixIngr.textContent = `Condiments`;
+sixIngr.classList.add("item")
+console.log(sixIngr);
+list.append(sixIngr);
+
